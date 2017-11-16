ddf4aab2531c1e2c11ad9be47df33ed1e7b5b988
martorb <mario.torbado@alumnos.uva.es>
2017-11-16 10:35:03 +0100

    añado casos de test para RedAutobusesTest

--- srcTest/uva/tds/pr2/equipo05/RedAutobusesTest.java
+++ srcTest/uva/tds/pr2/equipo05/RedAutobusesTest.java
@@ -7,8 +7,41 @@ import org.junit.Test;
 public class RedAutobusesTest {
 
 	@Test
-	public void test() {
+	public void testDosLineas() { 
+		fail("Not yet implemented");
+	}
+	
+	public void testMenosDeDosLineas() { 
+		fail("Not yet implemented");
+	}
+	
+	public void testIdentifLineasUnicos() {
 		fail("Not yet implemented");
 	}
 
+	public void testIdentifLineasNoUnicos() {
+		fail("Not yet implemented");
+	}
+	
+	public void testDevuelveLineaQueExiste() {
+		fail("Not yet implemented");
+	}
+	
+	public void testNoDevuelveLineaPorqueNoExiste() {
+		fail("Not yet implemented");
+	}
+	
+	public void testNuevaLineaConExito() {
+		fail("Not yet implemented");
+	}
+	
+	public void testNuevaLineaNoValida(){ 
+		
+	}
+	
+	public void testDevuelveListadeLineasBien() {
+		fail("Not yet implemented");
+	}
+
+	
 }
