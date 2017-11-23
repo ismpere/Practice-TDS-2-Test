0c3230a246ed0a482d6ad6ae6b6cb8702c2f01d8
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-23 10:16:36 +0100

    Añado el javador del constructor

--- src/uva/tds/pr2/equipo05/GD.java
+++ src/uva/tds/pr2/equipo05/GD.java
@@ -1,7 +1,16 @@
 package uva.tds.pr2.equipo05;
 
+/**
+ * Implementacion de la clase GD, que es una direccion en grados decimales
+ * @author ismpere
+ * @author martorb
+ */
 public class GD{
-
+	/**
+	 * Cosntructor por defecto de GD
+	 * @param latitud
+	 * @param longitud
+	 */
 	public GD(double latitud, double longitud) {
 		// TODO Auto-generated constructor stub
 	}
