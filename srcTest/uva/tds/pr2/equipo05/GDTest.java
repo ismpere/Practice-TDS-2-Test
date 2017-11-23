9ab5e0447ee5eb648939484338ce2554a23b7ea8
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-23 10:18:35 +0100

    Añado los getter dentro del test para inicializar un GD

--- srcTest/uva/tds/pr2/equipo05/GDTest.java
+++ srcTest/uva/tds/pr2/equipo05/GDTest.java
@@ -5,11 +5,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * Implementacion de la clase test para GD
- * @author ismpere
- * @author martorb
- */
 public class GDTest {
 
 	@Test
