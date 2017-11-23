ed5cf9f8f1233dcd77dbfa51e72de99dbc4f381c
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-23 10:28:06 +0100

    Añado casos no validos para la clase GD

--- srcTest/uva/tds/pr2/equipo05/GDTest.java
+++ srcTest/uva/tds/pr2/equipo05/GDTest.java
@@ -14,5 +14,24 @@ public class GDTest {
 		assertEquals(gd1.getLatitud(), -180.00, 0.01);
 		assertEquals(gd1.getLongitud(), 180.00, 0.01);
 	}
+	
+	@Test (expected = AssertionError.class)
+	public void testInicializaGDNoValidoLatitudInferior(){
+		GD gd1 = new GD(-180.01, 180.00);
+	}
+	
+	@Test (expected = AssertionError.class)
+	public void testInicializaGDNoValidoLatitudSuperior(){
+		GD gd1 = new GD(180.02, 180.00);
+	}
+	@Test (expected = AssertionError.class)
+	public void testInicializaGDNoValidoLongitudInferior(){
+		GD gd1 = new GD(-180.00, -180.01);
+	}
+	
+	@Test (expected = AssertionError.class)
+	public void testInicializaGDNoValidoLongitudSuperior(){
+		GD gd1 = new GD(180.00, 180.01);
+	}
 
 }
