d836a0a83c6f2f42293919cc7f024d0710ae7bc1
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-23 10:42:12 +0100

    Añado una clase valida para hallar la distancia entre dos GD en metros y corrijo los valores limite de los casos de test

--- srcTest/uva/tds/pr2/equipo05/GDTest.java
+++ srcTest/uva/tds/pr2/equipo05/GDTest.java
@@ -9,29 +9,38 @@ public class GDTest {
 
 	@Test
 	public void testInicializaGDValido() {
-		GD gd1 = new GD(-180.00, 180.00);
+		GD gd1 = new GD(-179.99, 179.99);
 		
-		assertEquals(gd1.getLatitud(), -180.00, 0.01);
-		assertEquals(gd1.getLongitud(), 180.00, 0.01);
+		assertEquals(gd1.getLatitud(), -179.99, 0.01);
+		assertEquals(gd1.getLongitud(), 179.99, 0.01);
+	}
+	
+	@Test
+	public void testCalculaDistanciaEntreDosGDValido(){
+		GD gd1 = new GD(-179.99, 179.99);
+		GD gd2 = new GD(179.99, -179.99);
+		
+		assertEquals(GD.getDistanciaEntre(gd1, gd2), 200.00, 0.01); //TODO Es un valor aleatorio, comprobar al implementar
 	}
 	
 	@Test (expected = AssertionError.class)
 	public void testInicializaGDNoValidoLatitudInferior(){
-		GD gd1 = new GD(-180.01, 180.00);
+		GD gd1 = new GD(-180.00, 179.99);
 	}
 	
 	@Test (expected = AssertionError.class)
 	public void testInicializaGDNoValidoLatitudSuperior(){
-		GD gd1 = new GD(180.02, 180.00);
+		GD gd1 = new GD(180.00, 179.99);
 	}
 	@Test (expected = AssertionError.class)
 	public void testInicializaGDNoValidoLongitudInferior(){
-		GD gd1 = new GD(-180.00, -180.01);
+		GD gd1 = new GD(-179.99, -180.00);
 	}
 	
 	@Test (expected = AssertionError.class)
 	public void testInicializaGDNoValidoLongitudSuperior(){
-		GD gd1 = new GD(180.00, 180.01);
+		GD gd1 = new GD(179.99, 180.00);
 	}
+	
 
 }
