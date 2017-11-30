e774ecea8f652f0c500dbd03f0e9b77a4ce3bca8
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-30 11:01:15 +0100

    Añado el javadoc del constructor por defecto

--- src/uva/tds/pr2/equipo05/Linea.java
+++ src/uva/tds/pr2/equipo05/Linea.java
@@ -7,7 +7,11 @@ package uva.tds.pr2.equipo05;
  *
  */
 public class Linea{
-
+	/**
+	 * Constructor por defecto de la clase Linea
+	 * @param string
+	 * @param p
+	 */
 	public Linea(String string, Parada[] p) {
 		// TODO Auto-generated constructor stub
 	}
