951e9fb8fa5120581ca1ec1aaa71d97fe7ee2dd5
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-30 09:56:07 +0100

    Añado el @before y @after creando el GD y eliminandolo de los test

--- srcTest/uva/tds/pr2/equipo05/ParadaTest.java
+++ srcTest/uva/tds/pr2/equipo05/ParadaTest.java
@@ -2,13 +2,26 @@ package uva.tds.pr2.equipo05;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParadaTest {
+	
+	private GD gd1;
+	
+	@Before
+    public void setUp() throws Exception {
+        gd1 = new GD (-179.99,179.99);
+    }
+     
+    @After
+    public void tearDown() throws Exception {
+        gd1 = null;
+    }
 
 	@Test
 	public void testInicializaParadasValido() {
-		GD gd1= new GD (-179.99,179.99);
 		Parada p1= new Parada(1, gd1);
 		
 		assertNotNull(p1);
@@ -18,7 +31,6 @@ public class ParadaTest {
 	
 	@Test
 	public void testSetIDValido(){
-		GD gd1= new GD (-179.99,179.99);
 		Parada p1= new Parada(1, gd1);
 		
 		p1.setId(2);
