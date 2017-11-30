903ea8c91abb6dcac88468c7755334a621bd2d05
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-30 09:52:09 +0100

    Añado el test setID valido

--- srcTest/uva/tds/pr2/equipo05/ParadaTest.java
+++ srcTest/uva/tds/pr2/equipo05/ParadaTest.java
@@ -17,4 +17,14 @@ public class ParadaTest {
 	}
 	
 	@Test
+	public void testSetIDValido(){
+		GD gd1= new GD (-179.99,179.99);
+		Parada p1= new Parada(1, gd1);
+		
+		p1.setId(2);
+		
+		assertNotNull(p1);
+		assertEquals(2,p1.getId());
+		assertEquals(gd1,p1.getGD());
+	}
 }
