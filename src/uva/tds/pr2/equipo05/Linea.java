db4713a62349eceaf52d5cf1e91d008ab8eb07f3
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-11-30 13:17:53 +0100

    Añado los getter autogenerados por los test

--- src/uva/tds/pr2/equipo05/Linea.java
+++ src/uva/tds/pr2/equipo05/Linea.java
@@ -9,11 +9,21 @@ package uva.tds.pr2.equipo05;
 public class Linea{
 	/**
 	 * Constructor por defecto de la clase Linea
-	 * @param string
+	 * @param id
 	 * @param p
 	 */
-	public Linea(String string, Parada[] p) {
+	public Linea(String id, Parada[] p) {
 		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Parada[] getParadas() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
