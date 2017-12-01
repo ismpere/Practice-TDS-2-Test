fbda71d264dedb51d7b7211f63130ec7753d8e34
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-01 23:48:28 +0100

    Separo los test añadiendo una nueva fixture, LineaGetParadasCercanasTest, asi siendo mas facil manejar los test

---
+++ srcTest/uva/tds/pr2/equipo05/LineaGetParadasCercanasTest.java
@@ -0,0 +1,90 @@
+package uva.tds.pr2.equipo05;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LineaGetParadasCercanasTest {
+	
+	private GD gd1 = new GD (-179.99,179.99);
+	private GD gd2 = new GD (-178.99,178.99);
+	private GD gd3 = new GD (-177.99,177.99);
+	private Parada p1, p2, p3;
+	private Parada p[] = {p1, p2, p3};
+	private Linea l1;
+	
+	@Before
+    public void setUp() throws Exception {
+        p1 = new Parada("a", gd1);
+        p2 = new Parada("b", gd2);
+        p3 = new Parada("c", gd3);
+        l1 = new Linea(1, p);
+    }
+     
+    @After
+    public void tearDown() throws Exception {
+        p1 = null;	
+        p2 = null;	
+        p3 = null;
+        p = null;
+        l1 = null;
+    }
+
+	@Test
+	public void testGetParadaCercanaUnaParadaCercanaValido(){
+		
+		GD gd4 = new GD(-150.00, 150.00); //TODO son ubicaciones aleatorias, poner una valida que lo cumpla
+		
+		Parada pa2[] = l1.getParadasCercanas(gd4);
+		
+		assertNotNull(l1);
+		assertNotNull(pa2);
+		assertTrue(l1.existeParadasCercanas(gd4));
+		assertTrue(pa2.length==1);
+		assertEquals(p2, pa2[0]);
+		assertEquals(1, l1.getId());
+		assertEquals(p1, l1.getParadaInicio());
+		assertEquals(p3, l1.getParadaFin());
+		assertArrayEquals(p, l1.getParadas());
+	}
+	
+	@Test
+	public void testGetParadaCercanaVariasParadasCercanasValido(){
+		
+		GD gd4 = new GD(-140.00, 140.00); //TODO son ubicaciones aleatorias, poner una valida que lo cumpla
+		
+		Parada pa2[] = l1.getParadasCercanas(gd4);
+		
+		Parada pa3[] = {p2, p3};
+		
+		assertNotNull(l1);
+		assertNotNull(pa2);
+		assertTrue(l1.existeParadasCercanas(gd4));
+		assertTrue(pa2.length==2);
+		assertEquals(1, l1.getId());
+		assertEquals(p1, l1.getParadaInicio());
+		assertEquals(p3, l1.getParadaFin());
+		assertArrayEquals(p, l1.getParadas());
+		assertArrayEquals(pa3, pa2);
+	}
+	
+	@Test
+	public void testGetParadaCercanaNingunaParadaCercanaValido(){
+		
+		GD gd4 = new GD(-130.00, 130.00); //TODO son ubicaciones aleatorias, poner una valida que lo cumpla
+		
+		Parada pa2[] = l1.getParadasCercanas(gd4);
+		
+		assertNotNull(l1);
+		assertNotNull(pa2);
+		assertFalse(l1.existeParadasCercanas(gd4));
+		assertTrue(pa2.length==0);
+		assertEquals(1, l1.getId());
+		assertEquals(p1, l1.getParadaInicio());
+		assertEquals(p3, l1.getParadaFin());
+		assertArrayEquals(p, l1.getParadas());
+	}
+
+}
