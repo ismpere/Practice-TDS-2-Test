c418ddcba10508f4df2447457a46abe6cbc84127
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-01 13:13:36 +0100

    Añado un caso valido para añadir una parada en la posicion deseada

--- srcTest/uva/tds/pr2/equipo05/LineaTest.java
+++ srcTest/uva/tds/pr2/equipo05/LineaTest.java
@@ -1,7 +1,6 @@
 package uva.tds.pr2.equipo05;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
@@ -12,13 +11,13 @@ public class LineaTest {
 	private GD gd2 = new GD (-178.99,178.99);
 	private GD gd3 = new GD (-177.99,177.99);
 	private Parada p1, p2, p3;
+	private Parada p[] = {p1, p2, p3};
 
 	@Before
     public void setUp() throws Exception {
         p1 = new Parada("a", gd1);
         p2 = new Parada("b", gd2);
         p3 = new Parada("c", gd3);
-        Parada p[] = {p1, p2, p3};
     }
      
     @After
@@ -84,6 +83,23 @@ public class LineaTest {
 		l1.addParadaIntermedia(p4);
 		
 		Parada pa2[] = {p1, p4, p2, p3};
+		assertNotNull(l1);
+		assertEquals("1", l1.getId());
+		assertEquals(p1, l1.getParadaInicio());
+		assertEquals(p3, l1.getParadaFin());
+		assertArrayEquals(pa2, l1.getParadas());
+	}
+	
+	@Test
+	public void testAñadeParadaEnPosicionValido(){
+		Linea l1 = new Linea("1", p);
+		
+		GD gd4 = new GD(-150.00, 150.00);
+		Parada p4 = new Parada("d", gd4);
+		
+		l1.addParadaAt(p4, 3);
+		
+		Parada pa2[] = {p1, p2, p4, p3};
 		assertNotNull(l1);
 		assertEquals("1", l1.getId());
 		assertEquals(p1, l1.getParadaInicio());
