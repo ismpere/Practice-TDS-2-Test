eda465fdee28baaa1ac991a22a3a000154856288
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-02 00:37:03 +0100

    Añado una clase de pruebas para hacer las pruebas de caja negra

---
+++ srcTest/uva/tds/pr2/equipo05/LineaBlackBoxTest.java
@@ -0,0 +1,32 @@
+package uva.tds.pr2.equipo05;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LineaBlackBoxTest {
+
+	private GD gd1 = new GD (-179.99,179.99);
+	private GD gd2 = new GD (-178.99,178.99);
+	private GD gd3 = new GD (-177.99,177.99);
+	private Parada p1, p2, p3;
+	private Parada p[] = {p1, p2, p3};
+
+	@Before
+    public void setUp() throws Exception {
+        p1 = new Parada("a", gd1);
+        p2 = new Parada("b", gd2);
+        p3 = new Parada("c", gd3);
+    }
+     
+    @After
+    public void tearDown() throws Exception {
+        p1 = null;	
+        p2 = null;	
+        p3 = null;
+        p = null;
+    }
+
+}
