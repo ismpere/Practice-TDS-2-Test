32f8af9ee2b8c09635c25d503ad019a645b400f6
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-02 14:18:03 +0100

    Añado una suite para ejecutar todos los test de GD

---
+++ srcTest/uva/tds/pr2/equipo05/GDAllTest.java
@@ -0,0 +1,19 @@
+package uva.tds.pr2.equipo05;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+ 
+@RunWith(Suite.class)
+@SuiteClasses({ 
+    GDTDDTest.class,
+    GDBlackBoxTest.class,})
+
+/**
+ * Suite de clases de test para todas las clases de test de Linea*
+ * @author ismael
+ * @author martorb
+ */
+public class GDAllTest {
+ 
+}
