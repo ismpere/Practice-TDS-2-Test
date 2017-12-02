9230821f354b7d721b498f4f6b76add30fc99de3
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-02 02:23:55 +0100

    Añado una suite para las clases de test getParadas*

---
+++ srcTest/uva/tds/pr2/equipo05/LineaGetParadasTest.java
@@ -0,0 +1,20 @@
+package uva.tds.pr2.equipo05;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+ 
+@RunWith(Suite.class)
+@SuiteClasses({ 
+    LineaGetParadasCercanasTest.class, 
+    LineaGetParadasConCorrespondenciaTest.class,
+    LineaGetParadasConTransbordoDirectoTest.class})
+
+/**
+ * Suite de clases de test para las getParadas*
+ * @author ismael
+ * @author martorb
+ */
+public class LineaGetParadasTest {
+ 
+}
