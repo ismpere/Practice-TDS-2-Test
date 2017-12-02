d209f6dcdd7c0f785bb87306dd66cfed49889d9c
Mario <mariotorbado.96@gmail.com>
2017-12-02 15:58:48 +0100

    Añado javadoc a la clase RedAutobuses
    cabecera, constructor y método getLinea

--- src/uva/tds/pr2/equipo05/RedAutobuses.java
+++ src/uva/tds/pr2/equipo05/RedAutobuses.java
@@ -1,13 +1,23 @@
 package uva.tds.pr2.equipo05;
 
+/**
+ * 
+ * @author martorb
+ * @author ismpere
+ *
+ */
 public class RedAutobuses {
 	
-	
+	/**
+	 * Constructor por defecto de la clase RedAutobuses
+	 * @param lista_lineas
+	 */
 	public RedAutobuses(Linea[] lista_lineas){
-		
+		// TODO Auto-generated constructor stub
 	}
 	
 	public Linea getLinea(String id){
+		// TODO Auto-generated constructor stub
 		return null;
 	}
 	
