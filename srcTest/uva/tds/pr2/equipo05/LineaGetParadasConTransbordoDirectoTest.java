3c0bfc7bace0109c94951337d31666b803e783dd
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-02 01:57:02 +0100

    Añado una clase de test para getParadasConTransbordoDirecto y corrijo unos asertos de las anteriores clases de test

--- srcTest/uva/tds/pr2/equipo05/LineaGetParadasConTransbordoDirectoTest.java
+++ srcTest/uva/tds/pr2/equipo05/LineaGetParadasConTransbordoDirectoTest.java
@@ -6,8 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class LineaGetParadasConCorrespondenciaTest {
-	
+public class LineaGetParadasConTransbordoDirectoTest {
+
 	private GD gd1 = new GD (-179.99,179.99);
 	private GD gd2 = new GD (-178.99,178.99);
 	private GD gd3 = new GD (-177.99,177.99);
@@ -33,7 +33,7 @@ public class LineaGetParadasConCorrespondenciaTest {
     }
     
     @Test
-	public void testGetParadasConCorrespondenciaUnaParadaConCorrespondenciaValido(){
+	public void testGetParadasConTransbordoDirectoUnaParadaConTDValido(){
     	
     	GD gd4 = new GD(-100.00, 100.00);
     	Parada p4 = new Parada("d", gd4);
@@ -43,11 +43,11 @@ public class LineaGetParadasConCorrespondenciaTest {
 		Parada pa3[] = {p1, p4, p5}; //TODO son paradas aleatorias, poner paradas que lo cumplan
 		Linea l2 = new Linea(2, pa3);
 		
-		Parada pcc[] = l1.getParadasConCorrespondencia(l2);
+		Parada pcc[] = l1.getParadasConTransbordoDirecto(l2);
 		
 		assertNotNull(l1);
 		assertNotNull(pcc);
-		assertTrue(l1.existeCorrespondencia(l2));
+		assertTrue(l1.existeTransbordoDirecto(l2));
 		assertEquals(1, pcc.length);
 		assertEquals(p1, pcc[0]);
 		assertEquals(1, l1.getId());
@@ -57,7 +57,7 @@ public class LineaGetParadasConCorrespondenciaTest {
 	}
 	
 	@Test
-	public void testGetParadasConCorrespondenciaVariasParadasConCorrespondenciaValido(){
+	public void testGetParadasConTransbordoDirectoVariasParadasConTDValido(){
 		
 		GD gd4 = new GD(-100.00, 100.00);
     	Parada p4 = new Parada("d", gd4);
@@ -65,13 +65,13 @@ public class LineaGetParadasConCorrespondenciaTest {
 		Parada pa3[] = {p1, p4, p3}; //TODO son paradas aleatorias, poner paradas que lo cumplan
 		Linea l2 = new Linea(2, pa3);
 		
-		Parada pcc[] = l1.getParadasConCorrespondencia(l2);
+		Parada pcc[] = l1.getParadasConTransbordoDirecto(l2);
 		
 		Parada pa4[] = {p1, p3};
 		
 		assertNotNull(l1);
 		assertNotNull(pcc);
-		assertTrue(l1.existeCorrespondencia(l2));
+		assertTrue(l1.existeTransbordoDirecto(l2));
 		assertEquals(2, pcc.length);
 		assertEquals(1, l1.getId());
 		assertEquals(p1, l1.getParadaInicio());
@@ -81,7 +81,7 @@ public class LineaGetParadasConCorrespondenciaTest {
 	}
 	
 	@Test
-	public void testGetParadasConCorrespondenciaNingunaParadaConCorrespondenciaValido(){
+	public void testGetParadasConTransbordoDirectoNingunaParadaConTDValido(){
 		
 		GD gd4 = new GD(-100.00, 100.00);
     	Parada p4 = new Parada("d", gd4);
@@ -93,11 +93,11 @@ public class LineaGetParadasConCorrespondenciaTest {
 		Parada pa3[] = {p4, p5, p6}; //TODO son paradas aleatorias, poner paradas que lo cumplan
 		Linea l2 = new Linea(2, pa3);
 		
-		Parada pcc[] = l1.getParadasConCorrespondencia(l2);
+		Parada pcc[] = l1.getParadasConTransbordoDirecto(l2);
 		
 		assertNotNull(l1);
 		assertNotNull(pcc);
-		assertFalse(l1.existeCorrespondencia(l2));
+		assertFalse(l1.existeTransbordoDirecto(l2));
 		assertEquals(0, pcc.length);
 		assertEquals(1, l1.getId());
 		assertEquals(p1, l1.getParadaInicio());
