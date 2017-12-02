0623f198a90639919d869f69176e90b2675cde50
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-02 01:06:38 +0100

    Añado casos no validos para addParadaIntermedia y añado algunos mas no validos para los casos anteriores

--- srcTest/uva/tds/pr2/equipo05/LineaBlackBoxTest.java
+++ srcTest/uva/tds/pr2/equipo05/LineaBlackBoxTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class LineaBlackBoxTest {
 
-	private GD gd1 = new GD (-179.99,179.99);
+	private GD gd1 = new GD (-179.99,179.99); //TODO las direcciones hay que ajustarlas, son aleatorias
 	private GD gd2 = new GD (-178.99,178.99);
 	private GD gd3 = new GD (-177.99,177.99);
 	private Parada p1, p2, p3;
@@ -40,10 +40,7 @@ public class LineaBlackBoxTest {
     
     @Test (expected = AssertionError.class)
     public void testInicializaNoValidoInicioYFinDemasiadoLejos(){
-    	GD gd4 = new GD(-100.00, 100.00);
-    	Parada p4 = new Parada("d", gd4);
-    	
-    	Parada p[] = {p1, p2, p4};
+    	Parada p[] = {p1, p3, p2};
     	Linea l1 = new Linea(1, p);
     }
     
@@ -81,6 +78,13 @@ public class LineaBlackBoxTest {
     	
     	l1.setParadaInicio(null);
     }
+    @Test (expected = IllegalArgumentException.class)
+    public void testsetParadaInicioNoValidoYaExiste(){
+    	Parada p[] = {p1, p2, p3};
+    	Linea l1 = new Linea(1, p);
+    	
+    	l1.setParadaInicio(p1);
+    }
     @Test (expected = AssertionError.class)
     public void testsetParadaFinNoValidoDemasiadoLejos(){
     	Parada p[] = {p1, p2, p3};
@@ -97,6 +101,27 @@ public class LineaBlackBoxTest {
     	Linea l1 = new Linea(1, p);
     	
     	l1.setParadaFin(null);
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void testsetParadaFinNoValidoYaExiste(){
+    	Parada p[] = {p1, p2, p3};
+    	Linea l1 = new Linea(1, p);
+    	
+    	l1.setParadaInicio(p3);
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void testaddParadaIntermediaNoValidoNulo(){
+    	Parada p[] = {p1, p2, p3};
+    	Linea l1 = new Linea(1, p);
+    	
+    	l1.addParadaIntermedia(null);
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void testaddParadaIntermediaNoValidoYaExiste(){
+    	Parada p[] = {p1, p2, p3};
+    	Linea l1 = new Linea(1, p);
+    	
+    	l1.addParadaIntermedia(p1);
     }
 
 }
