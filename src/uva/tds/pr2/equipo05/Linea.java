f3a5e50b9a7b6f0eb8062fb8c0e5861fe8f45426
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-02 12:58:15 +0100

    Mejoro el javadoc añadiendo los asser.pre y otros detalles

--- src/uva/tds/pr2/equipo05/Linea.java
+++ src/uva/tds/pr2/equipo05/Linea.java
@@ -9,15 +9,18 @@ package uva.tds.pr2.equipo05;
 public class Linea{
 	/**
 	 * Constructor por defecto de la clase Linea
-	 * @param id
-	 * @param p
+	 * @param id de la Linea
+	 * @param p Paradas de la linea
+	 * @assert.pre p.length>3
+	 * @assert.pre p[i]!=p[j] / i!=j Para todoas las paradas (No puede haber paradas repetidas)
+	 * @throws IllegalArgumentException si p=null || alguna de las paradas de p es null
 	 */
 	public Linea(int id, Parada[] p) {
 		// TODO Auto-generated constructor stub
 	}
 	/**
 	 * Devuelve el identificador de la linea
-	 * @return id
+	 * @return id de la Linea
 	 */
 	public int getId() {
 		// TODO Auto-generated method stub
@@ -25,7 +28,7 @@ public class Linea{
 	}
 	/**
 	 * Devuelve las paradas de la linea
-	 * @return paradas
+	 * @return paradas de la Linea
 	 */
 	public Parada[] getParadas() {
 		// TODO Auto-generated method stub
@@ -33,7 +36,7 @@ public class Linea{
 	}
 	/**
 	 * Devuelve la parada inicial de la linea
-	 * @return inicio
+	 * @return parada inicio
 	 */
 	public Parada getParadaInicio() {
 		// TODO Auto-generated method stub
@@ -41,7 +44,7 @@ public class Linea{
 	}
 	/**
 	 * Devuelve la parada final de la linea
-	 * @return fin
+	 * @return parada fin
 	 */
 	public Parada getParadaFin() {
 		// TODO Auto-generated method stub
@@ -49,8 +52,9 @@ public class Linea{
 	}
 	/**
 	 * Añade una nueva parada a la Linea
-	 * @param p
-	 * @param i
+	 * @param p Parada a añadir
+	 * @assert.pre !contains(p)
+	 * @throws IllegalArgumentException si p=null
 	 */
 	public void addParadaIntermedia(Parada p) {
 		// TODO Auto-generated method stub
@@ -58,7 +62,7 @@ public class Linea{
 	}
 	/**
 	 * Cambia el identificador de la linea
-	 * @param id
+	 * @param id Nuevo identificador de la Linea
 	 */
 	public void setId(int id) {
 		// TODO Auto-generated method stub
@@ -66,7 +70,10 @@ public class Linea{
 	}
 	/**
 	 * Cambia la parada ininio de la linea
-	 * @param p
+	 * @param p Nueva parada Inicio
+	 * @assert.pre !contains(p)
+	 * @assert.pre p.getDistanciaAt(getParadaFin)<100
+	 * @throws IllegalArgumentException si p=null
 	 */
 	public void setParadaInicio(Parada p) {
 		// TODO Auto-generated method stub
@@ -74,24 +81,34 @@ public class Linea{
 	}
 	/**
 	 * Cambia la parada fin de la linea
-	 * @param p
+	 * @param p Nueva parada Fin
+	 * @assert.pre !contains(p)
+	 * @assert.pre p.getDistanciaAt(getParadaInicio)<100
+	 * @throws IllegalArgumentException si p=null
 	 */
 	public void setParadaFin(Parada p) {
 		// TODO Auto-generated method stub
 		
 	}
 	/**
-	 * Añade la parada en la posicion seleccionada
-	 * @param p
-	 * @param i
+	 * Añade la parada intermedia en la posicion seleccionada
+	 * @param p Parada intermedia a añadir
+	 * @param i Posicion en la que se va a añadir
+	 * @assert.pre !contains(p)
+	 * @assert.pre i>1 && i<getParadas.length
+	 * @throws IllegalArgumentException si p=null
 	 */
-	public void addParadaAt(Parada p, int i) {
+	public void addParadaIntermediaAt(Parada p, int i) {
 		// TODO Auto-generated method stub
 		
 	}
 	/**
 	 * Elimina la parada intermedia de la linea
-	 * @param p
+	 * @param p Parada a eliminar
+	 * @assert.pre contains(p)
+	 * @assert.pre getParadas.length>3
+	 * @assert.pre !p.equals(getParadaInicio) && !p.equals(getParadaFin)
+	 * @throws IllegalArgumentException si p=null
 	 */
 	public void removeParadaIntermedia(Parada p) {
 		// TODO Auto-generated method stub
@@ -99,8 +116,10 @@ public class Linea{
 	}
 	/**
 	 * Devuelve una lista de paradas de la linea cercanas a una direccion
-	 * @param gd
-	 * @return paradas
+	 * La lista estara vacia si no hay ninguna parada cercana
+	 * @param gd Direccion desde la que queremos hallar las paradas cercanas
+	 * @return paradas cercanas a esa direccion
+	 * @throws IllegalArgumentException si gd=null
 	 */
 	public Parada[] getParadasCercanas(GD gd) {
 		// TODO Auto-generated method stub
@@ -108,17 +127,21 @@ public class Linea{
 	}
 	/**
 	 * Devuelve si hay parads cercanas de esa linea a una direccion
-	 * @param gd
+	 * @param gd Direccion desde la que queremos comprobar si hay paradas cercanas
 	 * @return existenParadasCercanas
+	 * @throws IllegalArgumentException si gd=null
 	 */
 	public boolean existeParadasCercanas(GD gd) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 	/**
-	 * Devuelve las paradas con correspondencia de this con l
-	 * @param l
-	 * @return paradas
+	 * Devuelve la lista de paradas con correspondencia de this con l
+	 * Esta lista estara vacia si no hay ninguna parada con correspondencia con l
+	 * @param l Linea con la que queremos hallar las paradas con correspondencia
+	 * @return paradas con correspondencia de this con l
+	 * @assert.pre !equals(l)
+	 * @throws IllegalArgumentException si l=null
 	 */
 	public Parada[] getParadasConCorrespondencia(Linea l) {
 		// TODO Auto-generated method stub
@@ -126,17 +149,22 @@ public class Linea{
 	}
 	/**
 	 * Devuelve si hay o no correspondencia de this con l
-	 * @param l
+	 * @param l Linea con la que queremos hallar si hay correspondencia
 	 * @return hayCorrespondencia
+	 * @assert.pre !equals(l)
+	 * @throws IllegalArgumentException si l=null
 	 */
 	public boolean existeCorrespondencia(Linea l) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 	/**
-	 * Devuelve las paradas con transbordo directo de this con l
-	 * @param l
-	 * @return paradas
+	 * Devuelve la lista de paradas con transbordo directo de this con l
+	 * Esta lista estara vacia si no hay ninguna parada con transbordo directo
+	 * @param l Linea con la que queremos hallar las paradas con transbordo directo
+	 * @return paradas con transbordo directo
+	 * @assert.pre !equals(l)
+	 * @throws IllegalArgumentException si l=null
 	 */
 	public Parada[] getParadasConTransbordoDirecto(Linea l) {
 		// TODO Auto-generated method stub
@@ -144,17 +172,20 @@ public class Linea{
 	}
 	/**
 	 * Devuelve si hay transbordo directo entre this y l
-	 * @param l
+	 * @param l Linea con la que queremos hallar si hay transbordo directo
 	 * @return existeTransbordoDirecto
+	 * @assert.pre !equals(l)
+	 * @throws IllegalArgumentException si l=null
 	 */
 	public boolean existeTransbordoDirecto(Linea l) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 	/**
-	 * Devuelve si contiene una parada
-	 * @param p
-	 * @return contiene
+	 * Devuelve si la linea contiene una parada
+	 * @param p Parada a comprobar que contiene
+	 * @return contieneParada
+	 * @throws IllegalArgumentException si p=null
 	 */
 	public boolean contains(Parada p) {
 		// TODO Auto-generated method stub
