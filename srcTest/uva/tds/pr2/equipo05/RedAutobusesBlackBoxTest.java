7aa36546245074ab346cb8c56723253dca1e398d
Mario <mariotorbado.96@gmail.com>
2017-12-02 18:03:34 +0100

    Añado la clase RedAutobusesBlackBoxTest

---
+++ srcTest/uva/tds/pr2/equipo05/RedAutobusesBlackBoxTest.java
@@ -0,0 +1,14 @@
+package uva.tds.pr2.equipo05;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RedAutobusesBlackBoxTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
