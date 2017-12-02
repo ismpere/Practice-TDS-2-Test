e4d420e59575f93f1b37dc61ad638dc1350399cb
Mario <mariotorbado.96@gmail.com>
2017-12-02 19:00:31 +0100

    Añado los metodos @before y @after

--- srcTest/uva/tds/pr2/equipo05/RedAutobusesBlackBoxTest.java
+++ srcTest/uva/tds/pr2/equipo05/RedAutobusesBlackBoxTest.java
@@ -15,7 +15,42 @@ import org.junit.Test;
  *
  */
 public class RedAutobusesBlackBoxTest {
-
+	
+	private GD gd1 = new GD (-179.99,179.99);
+	private GD gd2 = new GD (-178.99,178.99);
+	private GD gd3 = new GD (-177.99,177.99);
+	private GD gd4 = new GD (-176.99,176.99);
+	private GD gd5 = new GD (-175.99,175.99);
+	private GD gd6 = new GD (-174.99,174.99);
+	
+	private Parada p1,p2,p3,p4,p5,p6;
+	private Parada p_1[] = {p1,p2,p3};
+	private Parada p_2[] = {p2,p3,p4};
+	private Parada p_3[] = {p4,p1,p2};
+	
+	private Linea l1, l2,l3;
+	
+	@Before
+    public void setUp() throws Exception {
+		
+		p1= new Parada("1",gd1);
+		p2= new Parada("2",gd2);
+		p3= new Parada("3",gd3);
+		l1= new Linea(1,p_1);
+		l2= new Linea(2,p_2);
+		l3= new Linea(3,p_3);
+	}
+	
+	@After
+	public void tearDown() throws Exception{
+		p1= null;
+		p2= null;
+		p3= null;
+		l1= null;
+		l2= null;
+		l3=null;
+	}
+	
 	@Test
 	public void testInicializaRedNoValidoSolo2Lineas() {
 		fail("Not yet implemented");
