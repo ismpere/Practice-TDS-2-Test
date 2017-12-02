efaf3a1ae8c420c7b778f6d5841c96762b720299
Mario <mariotorbado.96@gmail.com>
2017-12-02 18:04:22 +0100

    Añado imports y cabecera javadoc

--- srcTest/uva/tds/pr2/equipo05/RedAutobusesBlackBoxTest.java
+++ srcTest/uva/tds/pr2/equipo05/RedAutobusesBlackBoxTest.java
@@ -2,10 +2,22 @@ package uva.tds.pr2.equipo05;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
+
+import uva.tds.pr2.equipo05.*;
 import org.junit.Test;
 
+/**
+ * Implementacion de la clase de test de caja negra (incompleta) de la clase RedAutobuses
+ * @author martorb
+ * @author ismpere
+ *
+ */
 public class RedAutobusesBlackBoxTest {
 
+	
+	
 	@Test
 	public void test() {
 		fail("Not yet implemented");
