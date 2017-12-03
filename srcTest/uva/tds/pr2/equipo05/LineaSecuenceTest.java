e547b641a289c505b449d5d9b5454d616d804758
Ismael Pérez Martín <ismaelprzm@gmail.com>
2017-12-03 14:29:17 +0100

    Añado dos secuencias aleatorias para añadir paradas y los setter de Linea

--- srcTest/uva/tds/pr2/equipo05/LineaSecuenceTest.java
+++ srcTest/uva/tds/pr2/equipo05/LineaSecuenceTest.java
@@ -41,6 +41,11 @@ public class LineaSecuenceTest {
 		Linea l1 = new Linea(1, p);
 		
 		l1.removeParadaIntermedia(p2);
+		
+		Parada pa2[] = {p1,p2,p3};
+		
+		assertArrayEquals(pa2, l1.getParadas());
+		assertEquals(1, l1.getId());
 	}
 	
 	@Test (expected = AssertionError.class)
@@ -53,5 +58,74 @@ public class LineaSecuenceTest {
 		
 		l1.removeParadaIntermedia(p2);
 		l1.removeParadaIntermedia(p3);
+	}
+	
+	@Test
+	public void testLineaSecuenciaAleatoriaAñadeYEliminaParadasValido() {
+		Parada p[] = {p1,p2,p3};
+		Linea l1 = new Linea(1, p);
+		
+		GD gd4 = new GD(-160.00,160.00); //TODO las direcciones son aleatorias, poner direcciones que cumplan esto
+		Parada p4 = new Parada("d", gd4);
+		
+		l1.addParadaIntermedia(p4);
+		
+		GD gd5 = new GD(-110.00,110.00);
+		Parada p5 = new Parada("d", gd5);
+		
+		l1.addParadaIntermediaAt(p5, 3);
+		
+		Parada pa2[] = l1.getParadas();
+		Parada listaP1[] = {p1, p2, p5, p4, p3};
+		
+		l1.removeParadaIntermedia(p4);
+		Parada pa4[] = l1.getParadas();
+		Parada listaP2[] = {p1, p2, p5, p3};
+		
+		l1.removeParadaIntermedia(p5);
+		
+		Parada pa3[] = l1.getParadas();
+		
+		assertArrayEquals(p, pa3);
+		assertArrayEquals(listaP1, pa2);
+		assertArrayEquals(listaP2, pa4);
+		assertEquals(1, l1.getId());
+	}
+	
+	@Test
+	public void testLineaSecuenciaAleatoriaSettersValido(){
+		Parada p[] = {p1,p2,p3};
+		Linea l1 = new Linea(1, p);
+		
+		int id1 = l1.getId();
+		
+		GD gd4 = new GD(-160.00,160.00); //TODO las direcciones son aleatorias, poner direcciones que cumplan esto
+		Parada p4 = new Parada("d", gd4);
+		
+		l1.setParadaInicio(p4);
+		Parada pi1 = l1.getParadaInicio();
+		
+		Parada pa2[] = l1.getParadas();
+		Parada listaP1[] = {p4,p1,p2,p3};
+		
+		GD gd5 = new GD(-110.00,110.00);
+		Parada p5 = new Parada("d", gd5);
+		
+		l1.setParadaFin(p5);
+		Parada pf1 = l1.getParadaFin();
+		
+		Parada listaP2[] = {p4,p1,p2,p3,p5};
+		
+		l1.setId(3);
+		int id2 = l1.getId();
+		
+		l1.setId(1);
+		
+		assertEquals(id1, l1.getId());
+		assertEquals(p4, pi1);
+		assertEquals(p5, pf1);
+		assertNotEquals(id2, l1.getId());
+		assertArrayEquals(listaP1, pa2);
+		assertArrayEquals(listaP2, l1.getParadas());
 	}
 }
